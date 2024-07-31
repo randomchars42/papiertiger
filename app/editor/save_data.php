d259b70fcc7546dcdbe2ddc493bc691438e2b88a
randomchars42 <randomchars42@users.noreply.github.com>
2024-07-31 19:50:52 +0200

    make slightly more reusable

--- app/editor/save_data.php
+++ app/editor/save_data.php
@@ -27,44 +27,47 @@ set_error_handler('exceptions_error_handler');
 //echo file_get_contents("php://input");
 $content = json_decode(file_get_contents("php://input"));
 
-if (isset($_GET['file'])) {
-    $dir = getcwd() . '/../data';
-    $resource = $_GET['file'];
+function create_backup($dir, $file) {
     $date = new DateTimeImmutable();
     $stamp = $date->format('Y-m-d_H-i-s-v');
 
-    // create backup
-    $path = "{$dir}/{$resource}.json";
-    $path_backup = "{$dir}/{$resource}.bk.{$stamp}.json";
+    $path = "{$dir}/{$file}.json";
+    $path_backup = "{$dir}/{$file}.bk.{$stamp}.json";
     if (file_exists($path) && !copy($path, $path_backup)) {
         throw new Exception("Could not copy {$path} to {$path_backup}");
     }
 
     // delete old backups
-    $count = 0;
     $backups = [];
 
     if ($handle = opendir($dir)) {
-        while (($file = readdir($handle)) !== false) {
-            if (!in_array($file, array('.', '..')) &&
-                !is_dir($dir.$file) &&
-                substr($file, 0, strlen($resource) + 3) == "{$resource}.bk") {
-                $backups[] = $file;
-                $count++;
+        while (($entry = readdir($handle)) !== false) {
+            if (!in_array($entry, array('.', '..')) &&
+                !is_dir($dir.$entry) &&
+                substr($entry, 0, strlen($file) + 3) == "{$file}.bk") {
+                $backups[] = $entry;
             }
         }
     } else {
         throw new Exception("Could not open {$dir}");
     }
 
-    if ($count > 10) {
+    if (count($backups) > 10) {
         arsort($backups);
         $backups_to_remove = array_slice($backups, 10);
 
-        foreach ($backups_to_remove as $file) {
-            unlink("{$dir}/{$file}");
+        foreach ($backups_to_remove as $backup) {
+            unlink("{$dir}/{$backup}");
         }
     }
+}
+
+if (isset($_GET['file'])) {
+    $dir = getcwd() . '/../data';
+    $file = $_GET['file'];
+    $path = "{$dir}/{$file}.json";
+
+    create_backup($dir, $file);
 
     // prettify data
     $data = json_encode($content, JSON_PRETTY_PRINT);
